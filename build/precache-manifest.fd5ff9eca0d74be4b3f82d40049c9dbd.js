6c01720ca638223e2cc4c722ac7604055e499f99
papuht <papuht@utu.fi>
2020-04-08 12:49:01 +0300

    added frontend

---
+++ build/precache-manifest.fd5ff9eca0d74be4b3f82d40049c9dbd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "105be109497bf98642bf88bc0732f7ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ce8dc3e27c1087f4d3d",
+    "url": "/static/js/2.72b2c40e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.72b2c40e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d53970dcceab889414f",
+    "url": "/static/js/main.99cccca5.chunk.js"
+  },
+  {
+    "revision": "5cae70094d1da1591fe8",
+    "url": "/static/js/runtime-main.32c2e1c8.js"
+  }
+]);
