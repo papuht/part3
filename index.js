173756346f0fa8683a226dfcf2a8b53268a07d31
papuht <papuht@utu.fi>
2020-04-10 15:58:54 +0300

    update to db functionality

--- index.js
+++ index.js
@@ -42,11 +42,25 @@ app.get('/api/persons/:id', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
+  
+  /*
   const id = Number(request.params.id)
   persons = persons.filter(person => person.id !== id)
 
   response.status(204).end()
+  */
+  
+  Person
+    .findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => {
+      response.status(400).send({ error: 'malformatted id' })
+    })
 })
+  
+
 
 
 function getRandomInt(max) {
